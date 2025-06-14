a2fc1b84d57c2f197cd9862df7515ea290c95177
depay1221 <depay067@gmail.com>
2025-06-15 04:59:19 +0700

    Update rapki.js

--- rapki.js
+++ rapki.js
@@ -1,16 +1,4 @@
 {
-
-  "secretCode": "nika",
-
-  "description": "depaydev", 
-
-  "lastUpdated": "12/04/2025",
-
-  "author": "nika", 
-
-  "status": "Active",
-
-  "license": "MIT",
-
-  "version": "1.0"
+  "nikacode": "nika",
+  "description": "depaydev"
 }
